adc4a5da0462b8f3631a6535cdea1e9a604ac159
roqueiroz <rodrigogqueiroz@live.com>
2020-09-17 12:06:56 -0300

    criando projeto

---
+++ CampeoesDaCopa/CampeoesDaCopa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Admin Colaborador on 17/09/20.
+//  Copyright © 2020 Rodrigo Queiroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
